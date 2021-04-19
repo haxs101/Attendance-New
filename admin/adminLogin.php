155c3b4689ed04cef554bb6c84a1ab41caadafaf
Mael Baslote <mrbaslote@gmail.com>
2021-04-19 18:58:13 +0800

    change to navbar

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -160,6 +160,9 @@ if(isset($_POST['addTeacher'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="app.js"></script>
     <title>Admin Page</title>
@@ -177,108 +180,123 @@ if(isset($_GET['action']) && $_GET['action'] == 'error'){
 				}
 				?>
 <h1>Hello <?php echo htmlspecialchars($_SESSION["username"]); ?> </h1>
-    <div class="row align-items-center">
-        <div class="card">
-            <div class="card-body">
-                <button id="newTeacher" onclick="addTeacher()">New Teacher</button><br><br>
-                <button id="newStudent" onclick="addStudent()">New Student</button><br><br>
-                <button id="showTeacher" onclick="viewTeacher()">View Teacher</button><br><br>
-                <button id="attendance" onclick="viewAttendance()">Attendance</button><br><br>
-                <button><a href="../logout.php">Logout</a></button><br><br>
-            </div>
-        </div>
+
+<nav class="navbar navbar-light navbar-expand-lg" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="adminLogin.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" id="newTeacher" onclick="addTeacher()">New Teacher <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="showTeacher" onclick="viewTeacher()">View Teacher</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="attendance" onclick="viewAttendance()">Attendance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <div class="row">
+       
    
-        <div class="col-8" id="addTeacher"  style="display:none; padding-left:60px;">
+        <div class="col" id="addTeacher"  style="display:none; padding-left:60px;">
             
                
         <div class="wrapper">
-        <h2 style="text-align:center">New Teacher</h2>
-        
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-
-            <div class="form-group">
-                <label>Teacher Name</label>
-                <span class="invalid-feedback"><?php echo $added; ?></span> 
-                <input type="text" name="teacherName" class="form-control" required>
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-
-            <div class="form-group">
-                <label>ID Number</label>
-                <input type="text" name="idNumber" class="form-control" required>
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-
-            <div class="form-group">
-                <label>Address</label>
-                <input type="text" name="address" class="form-control" required>
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-
-            <div class="form-group">
-                <label>Contact</label>
-                <input type="text" name="contact" class="form-control" required>
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-
-
-          <div class="form-group">
-                <label>Email</label>
-                <input type="text" name="email" class="form-control" required>
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
-
-
-              
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
+            <h2 style="text-align:center">New Teacher</h2>
             
-            <div class="form-group">
-                <input type="submit" name="addTeacher" class="btn btn-primary" value="Submit">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <div class="form-group">
+                    <label>Teacher Name</label>
+                    <span class="invalid-feedback"><?php echo $added; ?></span> 
+                    <input type="text" name="teacherName" class="form-control" required>
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+                <div class="form-group">
+                    <label>ID Number</label>
+                    <input type="text" name="idNumber" class="form-control" required>
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+                <div class="form-group">
+                    <label>Address</label>
+                    <input type="text" name="address" class="form-control" required>
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+                <div class="form-group">
+                    <label>Contact</label>
+                    <input type="text" name="contact" class="form-control" required>
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+
+                 <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" class="form-control" required>
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+
                 
-            </div>
-            
-        </form>
-    </div>    
-               
-             
-      </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+                
+                <div class="form-group">
+                    <input type="submit" name="addTeacher" class="btn btn-primary" value="Submit">
+                    
+                </div>
+                
+            </form>
+        </div>    
+                
+                
+        </div>
 
 
-    <div class="col-8" id="addStudent" style="display:none;" >
-        <h1>Add a Student</h1>
-         Name: <input type="text"> <br>
-         ID #: <input type="text"><br>
-         Address:  <input type="text"><br>
-         Contact: <input type="text"><br>
-                 <input type="submit">
-     </div>
-   
- 
-
-    <div class="col-8" id="viewTeacher" style="display:none;" >
-         <h1>View Teacher</h1>
-           Name: <input type="text"> <br>
+        <div class="col" id="addStudent" style="display:none;" >
+            <h1>Add a Student</h1>
+            Name: <input type="text"> <br>
             ID #: <input type="text"><br>
-             Address:  <input type="text"><br>
-              Contact: <input type="text"><br>
-                <input type="submit">
-     </div>
-   
-
-
-    <div class="col-8" id="viewAttendance" style="display:none;" >
-        <h1>Attendace</h1>
-          Name: <input type="text"> <br>
-          ID #: <input type="text"><br>
             Address:  <input type="text"><br>
-                 Contact: <input type="text"><br>
-                 <input type="submit">
-     </div>
-   
+            Contact: <input type="text"><br>
+                    <input type="submit">
+        </div>
+    
+    
+
+        <div class="col" id="viewTeacher" style="display:none;" >
+            <h1>View Teacher</h1>
+            Name: <input type="text"> <br>
+                ID #: <input type="text"><br>
+                Address:  <input type="text"><br>
+                Contact: <input type="text"><br>
+                    <input type="submit">
+        </div>
+    
+
+
+        <div class="col" id="viewAttendance" style="display:none;" >
+            <h1>Attendace</h1>
+            Name: <input type="text"> <br>
+            ID #: <input type="text"><br>
+                Address:  <input type="text"><br>
+                    Contact: <input type="text"><br>
+                    <input type="submit">
+        </div>
+    
  
 </div>
 
--- teacher.php
+++ teacher.php
@@ -9,4 +9,39 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 
 ?>
-<a href="logout.php">Logout</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="app.js"></script>
+    <title>Teacher Page</title>
+</head>
+<nav class="navbar navbar-light navbar-expand-lg" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="teacher.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" id="newTeacher" onclick="addTeacher()">New Teacher <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="showTeacher" onclick="viewTeacher()">View Teacher</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="attendance" onclick="viewAttendance()">Attendance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
